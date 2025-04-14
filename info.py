c1bdae83ed5bca25d9608ae146a155b0931787bd
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-04-14 23:51:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,8 +11,8 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['9102574'])
-API_HASH = environ['0861a545777adba2d599304b6474aad1']
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
